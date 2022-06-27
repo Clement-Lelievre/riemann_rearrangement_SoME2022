0ed8e42e7bf75136f81682b16b86d19581ee13c8
clementlelievre <clement.lelievre90@gmail.com>
2022-06-27 20:36:18 +0200

    update favicon

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from utils import (
 
 st.set_page_config(
     page_title="Riemann magic",
-    page_icon="",
+    page_icon="➕",
     layout="centered",  # wide
     initial_sidebar_state="auto",  # collapsed
     menu_items={
